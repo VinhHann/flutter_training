e77cf45b97d4428b8431116af0468e492febd5dd
VinhHann <vinhhannguyen1995@gmail.com>
2022-11-15 21:13:01 +0700

    buoi 1
    bai tap buoi 1

---
+++ flutter_training_buoi_1/bin/flutter_training_buoi_1.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_training_buoi_1/flutter_training_buoi_1.dart' as flutter_training_buoi_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${flutter_training_buoi_1.calculate()}!');
+}
---
+++ flutter_training_buoi_1/test/flutter_training_buoi_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_training_buoi_1/flutter_training_buoi_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
